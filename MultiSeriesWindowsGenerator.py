41e7030848301676e50c12b1ef5c163f67d7e7fd
Paul Hosek <96589432+PaulHosek@users.noreply.github.com>
2023-04-10 15:29:18 +0200

    - window implemented, but not working yet. Current Bug: get tensor with too many nan

---
+++ MultiSeriesWindowsGenerator.py
@@ -0,0 +1,239 @@
+import os
+import datetime
+
+import IPython
+import IPython.display
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import tensorflow as tf
+from tensorflow.keras.utils import timeseries_dataset_from_array
+
+mpl.rcParams['figure.figsize'] = (8, 6)
+mpl.rcParams['axes.grid'] = False
+
+
+class MultiSeriesWindowsGenerator():
+    def __init__(self, input_width, label_width, shift, batch_size, label_columns=[], GROUPBY=None,
+                 regressor_columns=[], static_columns=[], DATE = "", LABELS = [""]):
+
+        self.batch_size = batch_size
+
+        # Work out the label column indices.
+        self.label_columns = label_columns
+        if len(label_columns) != 0:
+            self.label_columns_indices = {name: i for i, name in
+                                          enumerate(label_columns)}
+
+        self.GROUPBY = GROUPBY
+        self.regressor_columns = regressor_columns
+        self.static_columns = static_columns
+        self.DATE = DATE
+        self.LABELS = LABELS
+
+        # Work out the window parameters.
+        self.input_width = input_width
+        self.label_width = label_width
+        self.shift = shift
+
+        self.total_window_size = input_width + shift
+
+        self.input_slice = slice(0, input_width)
+        self.input_indices = np.arange(self.total_window_size)[self.input_slice]
+
+        self.label_start = self.total_window_size - self.label_width
+        self.labels_slice = slice(self.label_start, None)
+        self.label_indices = np.arange(self.total_window_size)[self.labels_slice]
+
+    def __repr__(self):
+        return '\n'.join([
+            f'Total window size: {self.total_window_size}',
+            f'Input indices: {self.input_indices}',
+            f'Label indices: {self.label_indices}',
+            f'Batch Size: {self.batch_size}',
+            f'Label column name(s): {self.label_columns}',
+            f'Additional Regressor column name(s): {self.regressor_columns}',
+            f'GROUPBY column(s): {self.GROUPBY}'
+        ])
+
+    def preprocess_dataset(self, data: pd.DataFrame):
+        try:
+            if np.vstack(data.index).shape[1] != 1:
+                data = data.reset_index()
+
+            by = self.GROUPBY + [self.DATE]
+            labels = self.label_columns + self.regressor_columns + self.static_columns
+            data = data.set_index(by).unstack(-1)
+            data.fillna(0, inplace=True)
+            data = tf.stack([data[label] for label in labels], axis=-1)
+            print("THis is the data tensor\n", data)
+            if data.ndim != 3:
+                data = data[None, None, tf.newaxis]
+        except Exception as e:
+            print('Error while processing dataset', e)
+        return data
+
+    def update_datasets(self, train_df: pd.DataFrame, val_df: pd.DataFrame, test_df: pd.DataFrame, norm: bool = False):
+        # Store the raw data.
+        self.train_df = self.preprocess_dataset(train_df)
+        self.val_df = self.preprocess_dataset(val_df)
+        self.test_df = self.preprocess_dataset(test_df)
+
+        if norm:
+            train_mean = tf.reduce_mean(self.train_df, axis=1, keepdims=True)
+            train_std = tf.math.reduce_std(self.train_df, axis=1, keepdims=True)
+
+            self.train_df = (self.train_df - train_mean) / train_std
+            self.val_df = (self.val_df - train_mean) / train_std
+            self.test_df = (self.test_df - train_mean) / train_std
+
+            self.train_mean = train_mean
+            self.train_std = train_std
+            self.norm = norm
+
+        labels = self.label_columns + self.regressor_columns + self.static_columns
+        self.column_indices = {name: i for i, name in enumerate(labels)}
+
+    def preprocess_dataset(self, data: pd.DataFrame):
+        try:
+            if np.vstack(data.index).shape[1] != 1:
+                data = data.reset_index()
+
+            by = self.GROUPBY + [self.DATE]
+            labels = self.label_columns + self.regressor_columns + self.static_columns
+            data = data.set_index(by).unstack(-1)
+            data = tf.stack([data[label] for label in labels], axis=-1)
+
+            if data.ndim != 3:
+                data = data[None, None, tf.newaxis]
+        except Exception as e:
+            print('Error while processing dataset', e)
+        return data
+
+    def update_datasets(self, train_df: pd.DataFrame, val_df: pd.DataFrame,
+                        test_df: pd.DataFrame, norm: bool = False):
+        # Store the raw data.
+        self.train_df = self.preprocess_dataset(train_df)
+        self.val_df = self.preprocess_dataset(val_df)
+        self.test_df = self.preprocess_dataset(test_df)
+
+        if norm:
+            train_mean = tf.reduce_mean(self.train_df, axis=1, keepdims=True)
+            train_std = tf.math.reduce_std(self.train_df, axis=1, keepdims=True)
+
+            self.train_df = (self.train_df - train_mean) / train_std
+            self.val_df = (self.val_df - train_mean) / train_std
+            self.test_df = (self.test_df - train_mean) / train_std
+
+            self.train_mean = train_mean
+            self.train_std = train_std
+            self.norm = norm
+
+        labels = self.label_columns + self.regressor_columns + self.static_columns
+        self.column_indices = {name: i for i, name in enumerate(labels)}
+
+    def split_window(self, features: tf.Tensor):
+        inputs = features[:, self.input_slice, :]
+        labels = features[:, self.labels_slice, :]
+        if self.label_columns is not None:
+            labels = tf.stack(
+                [labels[:, :, self.column_indices[name]]
+                 for name in self.label_columns], axis=-1)
+
+        inputs.set_shape([None, self.input_width, None])
+        labels.set_shape([None, self.label_width, None])
+
+        return inputs, labels
+
+    def plot(self, inputs, labels, model=None, plot_col=None, max_subplots=3):
+        if not plot_col:
+            plot_col = self.LABELS[0]
+        plt.figure(figsize=(12, 8))
+        plot_col_index = self.column_indices[plot_col]
+        max_n = min(max_subplots, len(inputs))
+
+        for n in range(max_n):
+            plt.subplot(max_n, 1, n + 1)
+            plt.ylabel(f'{plot_col} {"[normed]" if self.norm else ""}')
+            plt.plot(self.input_indices, inputs[n, :, plot_col_index],
+                     label='Inputs', marker='.', zorder=-10)
+
+            if self.label_columns:
+                label_col_index = self.label_columns_indices.get(plot_col, None)
+            else:
+                label_col_index = plot_col_index
+
+            if label_col_index is None:
+                continue
+
+            plt.scatter(self.label_indices, labels[n, :, label_col_index],
+                        edgecolors='k', label='Labels', c='#2ca02c', s=64)
+            if model is not None:
+                predictions = model(inputs)
+                plt.scatter(self.label_indices, predictions[n, :, label_col_index],
+                            marker='X', edgecolors='k', label='Predictions',
+                            c='#ff7f0e', s=64)
+
+            if n == 0:
+                plt.legend()
+
+        plt.xlabel('Time [Days]')
+        plt.show()
+
+    def make_cohort(self, data: np.array) -> tf.data.Dataset:
+        data = np.array(data, dtype=np.float32)
+        ds = timeseries_dataset_from_array(
+            data=data,
+            targets=None,
+            sequence_length=self.total_window_size,
+            sequence_stride=1,
+            shuffle=True,
+            batch_size=self.batch_size
+        )
+        ds = ds.map(self.split_window)
+        return ds
+
+    """
+    Reference code from https://stackoverflow.com/questions/49994496/mixing-multiple-tf-data-dataset
+    """
+
+    def stack_windows(*windows):
+        features = tf.concat([window[0] for window in windows], 0)
+        labels = tf.concat([window[1] for window in windows], 0)
+        return (features, labels)
+
+    def make_dataset(self, data: tf.Tensor) -> tf.data.Dataset:
+        # num_cohorts = min(10, len(cluster_cohorts))
+        # print(cluster, num_cohorts)
+        ds_list = tuple(self.make_cohort(data[i]) for i in range(len(data)))
+        ds = tf.data.Dataset.zip(ds_list)
+        ds = ds.map(self.stack_windows)
+        ds = ds.unbatch()
+        ds = ds.shuffle(10, seed=0)
+        ds = ds.batch(self.batch_size)
+        ds = ds.prefetch(1)
+        return ds
+
+    @property
+    def train(self) -> tf.data.Dataset:
+        return self.make_dataset(self.train_df)
+
+    @property
+    def val(self) -> tf.data.Dataset:
+        return self.make_dataset(self.val_df)
+
+    @property
+    def test(self) -> tf.data.Dataset:
+        return self.make_dataset(self.test_df)
+
+    @property
+    def example(self):
+        """Get and cache an example batch of `inputs, labels` for plotting"""
+        result = getattr(self, '_example', None)
+        print('Number of train batches:', len(list(self.train.as_numpy_iterator())))
+        if result is None:
+            result = next(iter(self.train))
+            self._example = result
+        return result
