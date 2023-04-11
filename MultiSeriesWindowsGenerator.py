a9e4eab9eba5853692308b672add309365f61912
Paul Hosek <96589432+PaulHosek@users.noreply.github.com>
2023-04-11 14:49:26 +0200

    - all nan in rank 3 tensor gone

--- MultiSeriesWindowsGenerator.py
+++ MultiSeriesWindowsGenerator.py
@@ -15,7 +15,15 @@ from tensorflow.keras.utils import timeseries_dataset_from_array
 mpl.rcParams['figure.figsize'] = (8, 6)
 mpl.rcParams['axes.grid'] = False
 
+"""
+The MultiSeriesWindowsGenerator class generates windowed datasets for multivariate time series data. 
 
+The class works out the label column indices and window parameters based on the inputs. It then uses these parameters 
+to generate windowed datasets from the input data using the make_dataset and make_cohort methods. The split_window 
+method is used to split the windowed data into inputs and labels. 
+
+The preprocess_dataset method is used to preprocess the input data and stack a boolean mask to indicate where NaN 
+values are. The update_datasets method is used to update the stored raw data and normalize it if specified. """
 class MultiSeriesWindowsGenerator():
     def __init__(self, input_width, label_width, shift, batch_size, label_columns=[], GROUPBY=None,
                  regressor_columns=[], static_columns=[], DATE = "", LABELS = [""]):
@@ -59,16 +67,15 @@ class MultiSeriesWindowsGenerator():
             f'GROUPBY column(s): {self.GROUPBY}'
         ])
 
-    def preprocess_dataset_old(self, data: pd.DataFrame):
+    def preprocess_dataset(self, data: pd.DataFrame):
         try:
             if np.vstack(data.index).shape[1] != 1:
                 data = data.reset_index()
-
-            by = self.GROUPBY + [self.DATE]
+            by = self.GROUPBY + [self.DATE]  # [:-1]
             labels = self.label_columns + self.regressor_columns + self.static_columns
             data = data.set_index(by).unstack(-1)
             # FIXME need to handle new nan better here
-
+            #
             data.fillna(0, inplace=True)
             data = tf.stack([data[label] for label in labels], axis=-1)
             if data.ndim != 3:
@@ -77,36 +84,36 @@ class MultiSeriesWindowsGenerator():
             print('Error while processing dataset', e)
         return data
 
-    def preprocess_dataset(self, data: pd.DataFrame):
-        """
-        Preprocesses a pandas DataFrame containing time series data by
-         filling missing values with zeros and stacking a boolean mask
-        tensor to indicate where NaN values are.
-        Args:
-            data: A pandas DataFrame containing time series data.
-        Returns:
-            A tensorflow Tensor with shape [batch_size, time_steps, num_features + 1],
-             where the last dimension contains the
-            preprocessed data values and a boolean mask tensor indicating where NaN values are.
-        """
-        try:
-            if np.vstack(data.index).shape[1] != 1:
-                data = data.reset_index()
-
-            by = self.GROUPBY + [self.DATE]
-            labels = self.label_columns + self.regressor_columns + self.static_columns
-            data = data.set_index(by).unstack(-1)
-            mask = tf.cast(tf.math.is_nan(data), dtype=tf.float32)
-            data = tf.where(tf.math.is_nan(data), tf.zeros_like(data), data)
-
-            # Stack the data and mask tensors together
-            data = tf.stack([data[label] for label in labels] + [mask], axis=-1)
-
-            if data.ndim != 3:
-                data = data[None, None, tf.newaxis]
-        except Exception as e:
-            print('Error while processing dataset', e)
-        return data
+    # def preprocess_dataset_test(self, data: pd.DataFrame):
+    #     """
+    #     Preprocesses a pandas DataFrame containing time series data by
+    #      filling missing values with zeros and stacking a boolean mask
+    #     tensor to indicate where NaN values are.
+    #     Args:
+    #         data: A pandas DataFrame containing time series data.
+    #     Returns:
+    #         A tensorflow Tensor with shape [batch_size, time_steps, num_features + 1],
+    #          where the last dimension contains the
+    #         preprocessed data values and a boolean mask tensor indicating where NaN values are.
+    #     """
+    #     try:
+    #         if np.vstack(data.index).shape[1] != 1:
+    #             data = data.reset_index()
+    #
+    #         by = self.GROUPBY[:-1] + [self.DATE]
+    #         labels = self.label_columns + self.regressor_columns + self.static_columns
+    #         data = data.set_index(by).unstack(-1)
+    #         mask = tf.cast(tf.math.is_nan(data), dtype=tf.float32)
+    #         data = tf.where(tf.math.is_nan(data), tf.zeros_like(data), data)
+    #
+    #         # Stack the data and mask tensors together
+    #         data = tf.stack([data[label] for label in labels] + [mask], axis=-1)
+    #
+    #         if data.ndim != 3:
+    #             data = data[None, None, tf.newaxis]
+    #     except Exception as e:
+    #         print('Error while processing dataset', e)
+    #     return data
 
 
     def update_datasets(self, train_df: pd.DataFrame, val_df: pd.DataFrame, test_df: pd.DataFrame, norm: bool = False):
@@ -124,10 +131,10 @@ class MultiSeriesWindowsGenerator():
         self.val_df = self.preprocess_dataset(val_df)
         self.test_df = self.preprocess_dataset(test_df)
 
+
         if norm:
             train_mean = tf.reduce_mean(self.train_df, axis=1, keepdims=True)
             train_std = tf.math.reduce_std(self.train_df, axis=1, keepdims=True)
-
             self.train_df = (self.train_df - train_mean) / train_std
             self.val_df = (self.val_df - train_mean) / train_std
             self.test_df = (self.test_df - train_mean) / train_std
@@ -135,9 +142,21 @@ class MultiSeriesWindowsGenerator():
             self.train_mean = train_mean
             self.train_std = train_std
             self.norm = norm
+        nan_mask = tf.math.is_nan(self.train_df)
+        self.train_df = tf.where(nan_mask, tf.zeros_like(self.train_df), self.train_df)
+
+        nan_mask = tf.math.is_nan(self.val_df)
+        self.val_df = tf.where(nan_mask, tf.zeros_like(self.val_df), self.val_df)
+
+        nan_mask = tf.math.is_nan(self.test_df)
+        self.test_df = tf.where(nan_mask, tf.zeros_like(self.test_df), self.test_df)
 
         labels = self.label_columns + self.regressor_columns + self.static_columns
         self.column_indices = {name: i for i, name in enumerate(labels)}
+
+
+        mask = np.array(tf.math.is_nan(self.test_df))
+        # print(np.array(self.test_df[mask]))
 
     def split_window(self, features: tf.Tensor):
         """
@@ -265,8 +284,73 @@ class MultiSeriesWindowsGenerator():
     def example(self):
         """Get and cache an example batch of `inputs, labels` for plotting"""
         result = getattr(self, '_example', None)
-        print('Number of train batches:', len(list(self.train.as_numpy_iterator())))
         if result is None:
             result = next(iter(self.train))
             self._example = result
         return result
+
+
+if __name__ == "__main__":
+
+    data_list = []
+    for i in range(0, 27):
+        df = pd.read_csv(f"data/aggregated_individual_data_interpolation/interpolation/{i}_interpolated.csv",
+                         index_col=0)
+        df["subject_id"] = i + 1
+        data_list.append(df)
+
+    # Concatenate the data into a single dataset
+    data = pd.concat(data_list)
+    data.drop(["circumplex.arousal_std", "circumplex.valence_std", "mood_std", "activity_std"], inplace=True, axis=1)
+
+    use_date = 0
+
+    if use_date:
+        data.drop(["date"], inplace=True, axis=1)
+        date_time = pd.to_datetime(data.pop('date'))
+        df = data
+        df['days'] = date_time
+
+    else:  # use days from day 0 of recording
+        data.drop(["date"], inplace=True, axis=1)
+        df = data
+
+    df.reset_index(inplace=True, drop=True)
+    df = df.astype({'subject_id': 'float64', 'days': 'float64', 'weekday': 'float64'})
+    df = data
+
+    LABELS = ['mood']
+    REGRESSORS = ['weekday', 'circumplex.arousal', 'circumplex.valence',
+                  'activity', 'screen', 'call', 'sms', 'appCat.builtin',
+                  'appCat.communication', 'appCat.entertainment', 'appCat.finance',
+                  'appCat.game', 'appCat.office', 'appCat.other', 'appCat.social',
+                  'appCat.travel', 'appCat.unknown', 'appCat.utilities', 'appCat.weather']
+
+    DATE = 'days'  # always correct
+    IN_STEPS = 7  # use 7 days
+    OUT_STEPS = 7  # to predict 1 day in the future
+    GROUPBY = ['subject_id']
+    BATCH_SIZE = 8
+
+    n = len(df)
+    train_series = df.groupby(GROUPBY, as_index=False, group_keys=False).apply(
+        lambda x: x.iloc[:int(len(x) * 0.7)]).reset_index(drop=True)
+    val_series = df.groupby(GROUPBY, as_index=False, group_keys=False).apply(
+        lambda x: x.iloc[int(len(x) * 0.7):int(len(x) * 0.9)]).reset_index(drop=True)
+    test_series = df.groupby(GROUPBY, as_index=False, group_keys=False).apply(
+        lambda x: x.iloc[int(len(x) * 0.9):]).reset_index(drop=True)
+
+    test_window = MultiSeriesWindowsGenerator(
+        input_width=IN_STEPS, label_width=OUT_STEPS, shift=1, batch_size=BATCH_SIZE, GROUPBY=GROUPBY,
+        label_columns=LABELS, regressor_columns=REGRESSORS, DATE=DATE, LABELS=LABELS)
+
+    test_window.update_datasets(train_series, val_series, test_series, norm=True)
+    # print(test_window.test_df)
+    print("Are there nan in test:", np.array(tf.math.is_nan(test_window.test_df)).any())
+    print("Are there nan in test:", np.array(tf.math.is_nan(test_window.val_df)).any())
+    print("Are there nan in test:", np.array(tf.math.is_nan(test_window.train_df)).any())
+
+
+
+
+
